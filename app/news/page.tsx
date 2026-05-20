a7e20a37c3a9acdbd7b5cd46ec45389288262a85
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-20 14:01:42 +0200

    Add temporal dummy data to display

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
 
 function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
       <ul className="news-list">
-        <Link href={"/news/abc"}>Details news page</Link>
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
