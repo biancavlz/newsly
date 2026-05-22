da0f37b97030e536290b3afc9589ee2ae195da9d
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-22 11:45:54 +0200

    Fetch news from backend server API, removes dummy data

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,26 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-function NewsPage() {
+type NewsItem = {
+  id: string;
+  slug?: string;
+  title: string;
+  image?: string;
+  date: string;
+  content?: string;
+};
+
+async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
+  }
+  const news: NewsItem[] = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
