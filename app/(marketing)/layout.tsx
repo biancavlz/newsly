7226f4230c3afcc7e13a8c9ee36713d4c8028aa9
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-05-22 09:52:11 +0200

    Apply group routes () to show different layouts

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,19 @@
+import "../globals.css";
+
+export const metadata = {
+  title: "Newsly",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
